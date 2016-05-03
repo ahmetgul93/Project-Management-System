17d2b5a01a3f54cfe234e3c9a8f2dc754f3e6012
Ahmet Guel <ahmet.guel@mni.thm.de>
2016-05-03 22:46:04 +0300

    Student and GroupInfo classes are implemented. (( Bean ))

---
+++ Source/server.side.soft.tech.projectManagementSystem/Student.java
@@ -0,0 +1,38 @@
+
+public class Student implements Serializable{
+	private long int studentNumber;
+	private String name;
+	private String surname;
+	
+	
+	public Student()
+	{
+		
+	}
+	
+	public long int getstudentNumber()
+	{
+		return this.studentNumber;
+	}
+	public void setstudentNumber(long int studentNumber)
+	{
+		this.studentNumber = studentNumber;
+	}
+	public String getName()
+	{
+		return this.name;
+	}
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	public String getSurname()
+	{
+		return this.surname;
+	}
+	public void setSurname(String surname)
+	{
+		this.surname = surname;
+	}
+	
+}
