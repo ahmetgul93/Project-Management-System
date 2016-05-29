2b2858812e380fc5e453fec0b96f733cab3e1c7e
Ahmet Guel <ahmet.guel@mni.thm.de>
2016-05-30 02:45:06 +0300

    Parser !
    Türkçe karakterde patlıyor gibi.

--- Source/server.side.soft.tech.projectManagementSystem/src/mail/MessageParser.java
+++ Source/server.side.soft.tech.projectManagementSystem/src/mail/MessageParser.java
@@ -1,13 +1,30 @@
 package mail;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 public class MessageParser {
 
   public static void parse() {
     final List<String> messages = MessageManager.getMessages();
     for (final String message : messages) {
-
+    	Pattern pattern = Pattern.compile("[0-9]+-[A-z]+\\s[A-z]+"); 
+    	Matcher matcher = pattern.matcher(message);
+    	ArrayList<String> matches = new ArrayList<String>();
+    	while (matcher.find()) { 
+    		  // Get the matching string  
+    		  matches.add( matcher.group() );
+    		}
+    	
+    	for(String match: matches) // TEST
+    	{
+		 System.out.println(match + " ");
+    	}
+    	
     }
   }
 }
